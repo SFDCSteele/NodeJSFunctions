82741cf2b50c6199e9f540fdd050076e2128f14f
SFDCSteele <wsteele@salesforce.com>
2022-05-10 13:47:13 -0400

    trying to update

--- scripts/work.sh
+++ scripts/work.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-export devhub="NodeJSDevHub"
-export scratchOrg="WSteeleNodeJS"
-export DevHubUsername="wsteele@salesforce.com.functionsworkshop2"
+export devhub="JavaDevHub"
+export scratchOrg="WSteeleJava"
+export DevHubURL="http://functionworkshopcom.my.salesforce.com"
+export DevHubUsername="wsteele@salesforce.com.functionsworkshop3"
 export scratchOrgUserName="test-qluy4umhvpb5@example.com"
-export sandboxURL="https://functionsworkshop2com--f3wsteele.my.salesforce.com"
-export sandboxUsername="wsteele@salesforce.com.functionsworkshop2.F3wsteele"
+export sandboxURL="http://functionworkshopcom--f3wsteele.my.salesforce.com"
+export sandboxUsername="wsteele@salesforce.com.functionsworkshop3.F3wsteele"
 export sandboxAlias="F3wsteele"
 export sandboxCompute="F3wsteeleCompute"
 export functionOne="f3wsteelefunction"
+export gitRepo="git@github.com:SFDCSteele/NodeJSFunctions.git"
+export gitBranch="JavaFunction"
 
 dirs=( "activedirectory" "corefunctionality" "userprovisioning" )
+clear
 echo "==========================================================================="
 echo "Performing work.....$1 - Scratch Org: $scratchOrg"
 echo "==========================================================================="
@@ -22,9 +26,9 @@ sfdx force:org:list
 echo "1-Performing $1..."
 if [ "$1" = "auth" ]
 then
-    echo "logging into devHub..."
+    echo "logging into devHub...$devhub with URL: $DevHubURL user $DevHubUsername"
     sf login -d -a $devhub
-    echo "logging into functions..."
+    echo "logging into functions...$devhub with URL: $DevHubURL user $DevHubUsername"
     sf login functions
     sfdx auth:web:login -d -a $devhub
     echo "Once DevHub is authorized, hit return to continue..."
@@ -60,13 +64,20 @@ then
     ls -al functions/$functionOne
     sf run function start
     sf run function -l http://localhost:8080 -p "@functions/$functionOne/payload.json"
+elif [ "$1" = "gitininit" ]
+then
+    echo "Attempting to initialize git..$gitRepo"
+    git add remote -v $gitRepo
+    git fetch --all
+    git checkout $gitBranch
+    git status
 elif [ "$1" = "gitin" ]
 then
-    echo "Attempting to push to git.."
+    echo "Attempting to push to git..$gitBranch"
     git status
     git add .
     git commit -m "$2"
-    git push origin master
+    git push origin $gitBranch
 elif [ "$1" = "DEMO" ]
 then
     source ./scripts/deployMDAPI.sh $2
